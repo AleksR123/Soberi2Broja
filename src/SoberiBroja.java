e97e8a3d32146acaa8cfe301f2f746c56e3f1224
unknown <aleksandarristov28@gmail.com>
2023-12-23 18:22:06 +0100

    first java program

---
+++ src/SoberiBroja.java
@@ -0,0 +1,14 @@
+
+public class SoberiBroja {
+	
+	public static void main(String[] args) {
+		
+		int sum = Soberi_broevi(2,3,4);
+		System.out.println("Sumata na trite broja = " +sum);
+		
+	}
+      public static int Soberi_broevi(int a, int b, int c) {
+    	  return(a+b+c);
+      }
+
+}
